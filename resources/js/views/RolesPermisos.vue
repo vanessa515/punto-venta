93fc86e8f8fd3c5968346237a192439d7556edee
vane515 <rubiovanessa570@gmail.com>
2026-06-01 17:47:17 -0600

    first commit

---
+++ resources/js/views/RolesPermisos.vue
@@ -0,0 +1,9 @@
+<template>
+
+    <div>
+    
+        <h1>ROLES Y PERMISOS</h1>
+
+    </div>
+
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<title>Punto de venta</title>
+<title>Gestor Giro V2</title>
+<link rel="icon" href="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    <div id="app"></div>
+
+</body>
+</html>
