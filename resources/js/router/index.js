de04f772bccb7fb937dca7712953fa1d3ffcfa7f
Ruben5117 <rubenpraza@gmail.com>
2026-07-25 22:42:50 -0600

    cambio 1

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,9 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Inicio from '../views/Inicio.vue'
 import Categorias from '../views/Categorias.vue'
 import RolesPermisos from '../views/RolesPermisos.vue'
+import Usuarios from '../views/Usuarios.vue'
+import Sucursales from '../views/Sucursales.vue'
+import Login from '../views/Login.vue'
+import Menu from '../menu/sidebarmenu.vue'
 import Marcas from '../views/Marcas.vue'
 import Productos from '../views/Productos.vue'
-
 
 const routes = [
     {
