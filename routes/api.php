5d295b386e99d0ccf4e7dc02023ed72283eb3591
Ruben5117 <rubenpraza@gmail.com>
2026-08-08 20:52:27 -0600

    rutas

--- routes/api.php
+++ routes/api.php
@@ -46,3 +46,35 @@ Route::delete('/productos/{id}', [ProductoController::class, 'destroy']);
 Route::post('/productos/{id}/variantes', [ProductoController::class, 'agregarVariante']);
 Route::put('/variantes/{id}', [ProductoController::class, 'actualizarVariante']);
 Route::delete('/variantes/{id}', [ProductoController::class, 'eliminarVariante']);
+
+////////////////ROLES Y PERMISOS////////////////////
+
+Route::get('/roles', [RolesController::class, 'index']);
+
+Route::get('/permisos', [RolesController::class, 'permisos']);
+
+Route::post('/permisosinsert', [RolesController::class, 'store']);
+
+Route::put('/permisosupdate', [RolesController::class, 'update']);
+
+Route::delete('/permisosdelete', [RolesController::class, 'destroy']);
+
+////////////////USUARIOS////////////////////
+
+Route::get('/usuarios', [UsuariosController::class, 'index']);
+
+Route::post('/registrousr', [UsuariosController::class, 'store']);
+
+Route::put('/actualizarusr', [UsuariosController::class, 'update']);
+
+Route::delete('/usrdelete', [UsuariosController::class, 'destroy']);
+
+////////////////COMPANIAS////////////////////
+
+Route::get('/sucursales', [SucursalesController::class, 'index']);
+
+Route::post('/sucursales/store', [SucursalesController::class, 'store']);
+
+Route::put('/actualizar/comp', [SucursalesController::class, 'update']);
+
+Route::delete('/eliminar/comp', [SucursalesController::class, 'destroy']);
